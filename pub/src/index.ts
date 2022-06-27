1e418f1385699a3aba7059d93d6760c931097d40
corno <corno.astn.a@schraverus.com>
2022-06-27 12:42:38 +0200

    .

--- pub/src/index.ts
+++ pub/src/index.ts
@@ -6,7 +6,7 @@ import { printStructureError } from "./esc/imp/printStructureError"
 import { printTreeParserError } from "./esc/imp/printTreeParserError"
 import { printParsingError } from "./esc/imp/printParsingError"
 
-export const $/*: API*/ = {
+export const $: API = {
     createStructureParser: createStructureParser,
     createTreeParser: createTreeParser,
     createStructureErrorMessage: printStructureError,
