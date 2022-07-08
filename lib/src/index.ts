6b03f21c3995da0e3505e99a924c4e4461681f9f
corno <corno.astn.a@schraverus.com>
2022-07-08 07:26:53 +0200

    .

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -6,10 +6,12 @@ import { printStructureError } from "./esc/imp/printStructureError"
 import { printTreeParserError } from "./esc/imp/printTreeParserError"
 import { printParsingError } from "./esc/imp/printParsingError"
 
-export const $: API = {
-    createStructureParser: createStructureParser,
-    createTreeParser: createTreeParser,
-    createStructureErrorMessage: printStructureError,
-    createTreeParserErrorMessage: printTreeParserError,
-    createParsingErrorMessage: printParsingError,
+export function init(): API {
+    return {
+        createStructureParser: createStructureParser,
+        createTreeParser: createTreeParser,
+        createStructureErrorMessage: printStructureError,
+        createTreeParserErrorMessage: printTreeParserError,
+        createParsingErrorMessage: printParsingError,
+    }
 }
--- test/src/esc/bin/index.ts
+++ test/src/esc/bin/index.ts
@@ -1,3 +1,61 @@
-import * as pl from "pareto-lang-lib"
 
-pl.logDebugMessage("no tests yet")
+import * as lib from "../../../../lib"
+
+import * as pr from "pareto-runtime"
+
+import * as fslib from "pareto-filesystem-lib"
+import * as tok from "astn-tokenizer-lib"
+import * as testlib from "pareto-test-lib"
+import * as asyncLib from "pareto-async-lib"
+import * as diffLib from "pareto-diff-lib"
+import { getTests } from "../../imp/getTests"
+
+
+pr.runProgram(
+    ($) => {
+        if ($.argument === undefined) {
+            throw new Error("missing path")
+        }
+        const path = $.argument
+
+        const async = asyncLib.init()
+        const diff = diffLib.init()
+
+
+        const parserLib = lib.init()
+        const tokLib = tok.init()
+
+        const fs = fslib.init()
+
+
+        getTests(
+            path,
+            async.rewrite,
+            async.tuple2,
+            fs.directory,
+            fs.file,
+            tokLib.createTokenizer,
+            testlib.init(
+                fs,
+                diff,
+                async,
+            ).validateFile,
+        ).execute(($ => {
+            testlib.init(
+                fs,
+                diff,
+                async
+            ).serializeTestResult(
+                {
+                    testResult: $,
+                    showSummary: true,
+
+                },
+                (str) => {
+                    pr.log(str)
+                }
+            )
+        }))
+
+    }
+)
